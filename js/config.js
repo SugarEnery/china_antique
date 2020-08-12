0fc2c5abfbd9c7b12a41cfb128c0c58f38a64e72
SugarEnery <45059706+SugarEnery@users.noreply.github.com>
2020-08-12 15:18:36 +0800

    fix
    登录

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var _Config_ = {
+	API_PATH: "https://app.1yuaninfo.com/app/v1/"
+};
+
+// 线上地址
+var Router= "https://app.1yuaninfo.com/app_web/";
+
+// 本地地址
+// var Router = "http://192.168.23.123:8000/";
+
+ 
