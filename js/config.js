483fefae8e3f3c96e9663762a6f728eef152ecb8
SugarEnery <45059706+SugarEnery@users.noreply.github.com>
2020-08-24 00:23:07 +0800

    fix
    首页资讯列表

--- js/config.js
+++ js/config.js
@@ -1,13 +1,40 @@
-// var _Config_ = {
-// 	API_PATH: "http://api.chinabogu.com/"
-// };
+var _Config_ = {
+	API_PATH: "http://api.chinabogu.com"
+};
 
-// // 线上地址
-// var Router= "http://api.chinabogu.com/";
+// 线上地址
+var Router= "http://api.chinabogu.com";
 
-// // 本地地址
-// // var Router = "http://192.168.23.123:8000/";
-// //ajax
+// 本地地址
+// var Router = "http://192.168.23.123:8000/";
+//ajax
+function createAjax(ajaxrequire) {
+	// console.log(ajaxrequire)
+ 	_url = ajaxrequire.url;
+ 	// _dataType = ajaxrequire._dataType;
+ 	// _async = ajaxrequire._async;
+ 	_method = ajaxrequire.type;//请求
+ 	_data = ajaxrequire.data;
+ 	// _data.token = token;
+ 	// _data.device = 'weixin';
+
+    return $.ajax({
+        url: _url,
+        dataType: 'JSON',
+        cache: false,
+        // async: _async,
+        type: _method,
+        data: _data,
+        timeout:10000,
+		success:function(data){
+			// console.log(data);
+		},
+        error:function(err){
+			console.log(err)
+        	// mui.alert(data.msg);
+        }
+    });
+}
 // const ajaxPromise = (obj,access_token) => {
 //     return new Promise((resolve, reject) => {
 //         if(access_token){
